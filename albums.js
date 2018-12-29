c295ab057b9868e9767ec21ee14002148266c860
GregChan <gregorywarrenchan@gmail.com>
2018-12-29 00:36:21 -0500

    albums: script to extract albums from pipe separated csv

---
+++ albums.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const fs = require('fs');
+
+const file = fs.readFileSync('./songs.csv', 'utf-8');
+const lines = file.split('\n');
+
+const albums = new Set();
+
+lines.forEach(function(line) {
+  const re = /(?<song>.+)\|(?<artist>.+)\|(?<album>.+)/;
+  const result = re.exec(line);
+
+  if (!result) {
+    console.warn(line);
+    return;
+  }
+
+  console.log(result);
+  const {artist, album} = result.groups;
+
+  albums.add(`${album} - ${artist}`);
+});
+
+console.log(Array.from(albums).sort());
+console.log(albums.size);
